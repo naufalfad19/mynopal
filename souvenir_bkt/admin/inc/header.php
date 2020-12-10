3e1800a3692c3dc83dd15f5635d8d0873f8cec48
naufalfad19 <62590450+naufalfad19@users.noreply.github.com>
2020-12-10 20:43:00 +0700

    Update header.php

--- souvenir_bkt/admin/inc/header.php
+++ souvenir_bkt/admin/inc/header.php
@@ -1,7 +1,7 @@
 <div class="sidebar-toggle-box">
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
-             <a class="logo"><font face="Courier New">WebGis Souvenir (19 1811522021 Naufal Fadilan) </font></a>
+             <a class="logo"><font face="Courier New">WebGis Souvenir (21 1811522021 Naufal Fadilan) </font></a>
           
             <div class="top-menu">
               <ul class="nav pull-right top-menu" style="margin-top: 6px">
